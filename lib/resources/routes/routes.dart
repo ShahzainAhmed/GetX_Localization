f8b5437e6594c0fefcfa5702c290487abb1578dc
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2024-01-07 23:12:48 +0500

    added new route

--- lib/resources/routes/routes.dart
+++ lib/resources/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:getx_mvvm_mvc/resources/routes/route_names.dart';
+import 'package:getx_mvvm_mvc/view/localization_screen.dart';
 import 'package:getx_mvvm_mvc/view/login_screen.dart';
 import 'package:getx_mvvm_mvc/view/splash_screen.dart';
 
@@ -8,13 +9,19 @@ class AppRoutes {
         GetPage(
           name: RouteNames.splashScreen,
           page: () => const SplashScreen(),
-          transition: Transition.leftToRightWithFade,
+          transition: Transition.rightToLeftWithFade,
           transitionDuration: const Duration(milliseconds: 500),
         ),
         GetPage(
           name: RouteNames.loginScreen,
           page: () => const LoginScreen(),
-          transition: Transition.leftToRightWithFade,
+          transition: Transition.rightToLeftWithFade,
+          transitionDuration: const Duration(milliseconds: 500),
+        ),
+        GetPage(
+          name: RouteNames.languageLocalization,
+          page: () => const LanguageLocalization(),
+          transition: Transition.rightToLeftWithFade,
           transitionDuration: const Duration(milliseconds: 500),
         ),
       ];
